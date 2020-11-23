244982f106b59276268dd5f041b2c4346ade31ed
Akshay Gupta <kitallis@gmail.com>
2020-11-23 22:06:56 +0530

    turn on/off gpio pin

---
+++ README.md
@@ -0,0 +1,3 @@
+# wet-ass plants
+
+a small rust binary to schedule/control a drip-irrigation system – the code effectively just turns on/off a gpio pin (so you can probably do other simple things with it)
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+extern crate sysfs_gpio;
+
+use std::env;
+use std::thread::sleep;
+use std::time::Duration;
+use sysfs_gpio::{Direction, Pin};
+
+struct Arguments {
+  pin: u64,
+  duration_ms: u64,
+  period_ms: u64,
+}
+
+// Export a GPIO for use.  This will not fail if already exported
+fn blink_my_led(led: u64, duration_ms: u64, period_ms: u64) -> sysfs_gpio::Result<()> {
+  let my_led = Pin::new(led);
+  my_led.with_exported(|| {
+    my_led.set_direction(Direction::Low)?;
+    let iterations = duration_ms / period_ms / 2;
+    for _ in 0..iterations {
+      my_led.set_value(0)?;
+      sleep(Duration::from_millis(period_ms));
+      my_led.set_value(1)?;
+      sleep(Duration::from_millis(period_ms));
+    }
+    my_led.set_value(0)?;
+    Ok(())
+  })
+}
+
+fn print_usage() {
+  println!("Usage: ./blinky <pin> <duration_ms> <period_ms>");
+}
+
+fn get_args() -> Option<Arguments> {
+  let args: Vec<String> = env::args().collect();
+  if args.len() != 4 {
+    return None;
+  }
+  let pin = match args[1].parse::<u64>() {
+    Ok(pin) => pin,
+    Err(_) => return None,
+  };
+  let duration_ms = match args[2].parse::<u64>() {
+    Ok(ms) => ms,
+    Err(_) => return None,
+  };
+  let period_ms = match args[3].parse::<u64>() {
+    Ok(ms) => ms,
+    Err(_) => return None,
+  };
+  Some(Arguments {
+    pin: pin,
+    duration_ms: duration_ms,
+    period_ms: period_ms,
+  })
+}
+
+fn main() {
+  match get_args() {
+    None => print_usage(),
+    Some(args) => match blink_my_led(args.pin, args.duration_ms, args.period_ms) {
+      Ok(()) => println!("Success!"),
+      Err(err) => println!("We have a blinking problem: {}", err),
+    },
+  }
+}
