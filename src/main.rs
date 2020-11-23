0ddc77edab96f70324307518b686485a7a6535c4
Akshay Gupta <kitallis@gmail.com>
2020-11-24 01:07:44 +0530

    call it wap

--- src/main.rs
+++ src/main.rs
@@ -11,44 +11,51 @@ struct Arguments {
   period_ms: u64,
 }
 
-// Export a GPIO for use.  This will not fail if already exported
-fn blink_my_led(led: u64, duration_ms: u64, period_ms: u64) -> sysfs_gpio::Result<()> {
-  let my_led = Pin::new(led);
-  my_led.with_exported(|| {
-    my_led.set_direction(Direction::Low)?;
+// Export a GPIO for use. This will not fail if already exported
+fn pour(pin: u64, duration_ms: u64, period_ms: u64) -> sysfs_gpio::Result<()> {
+  let actionPin = Pin::new(pin);
+
+  actionPin.with_exported(|| {
+    actionPin.set_direction(Direction::Low)?;
     let iterations = duration_ms / period_ms / 2;
     for _ in 0..iterations {
-      my_led.set_value(0)?;
+      actionPin.set_value(0)?;
       sleep(Duration::from_millis(period_ms));
-      my_led.set_value(1)?;
+      actionPin.set_value(1)?;
       sleep(Duration::from_millis(period_ms));
     }
-    my_led.set_value(0)?;
+
+    actionPin.set_value(0)?;
     Ok(())
   })
 }
 
 fn print_usage() {
-  println!("Usage: ./blinky <pin> <duration_ms> <period_ms>");
+  println!("Usage: ./wap <pin> <duration_ms> <period_ms>");
 }
 
 fn get_args() -> Option<Arguments> {
   let args: Vec<String> = env::args().collect();
+
   if args.len() != 4 {
     return None;
   }
+
   let pin = match args[1].parse::<u64>() {
     Ok(pin) => pin,
     Err(_) => return None,
   };
+
   let duration_ms = match args[2].parse::<u64>() {
     Ok(ms) => ms,
     Err(_) => return None,
   };
+
   let period_ms = match args[3].parse::<u64>() {
     Ok(ms) => ms,
     Err(_) => return None,
   };
+
   Some(Arguments {
     pin: pin,
     duration_ms: duration_ms,
@@ -59,9 +66,9 @@ fn get_args() -> Option<Arguments> {
 fn main() {
   match get_args() {
     None => print_usage(),
-    Some(args) => match blink_my_led(args.pin, args.duration_ms, args.period_ms) {
+    Some(args) => match pour(args.pin, args.duration_ms, args.period_ms) {
       Ok(()) => println!("Success!"),
-      Err(err) => println!("We have a blinking problem: {}", err),
+      Err(err) => println!("error, dry-ass plants: {}", err),
     },
   }
 }
